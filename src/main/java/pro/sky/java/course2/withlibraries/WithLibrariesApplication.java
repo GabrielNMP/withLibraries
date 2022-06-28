bf25ad14081efc3d4107d345ad5e0e297bf23e25
Gabriel_n3 <p705865@gmail.com>
2022-06-28 13:27:14 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/withlibraries/WithLibrariesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.withlibraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithLibrariesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithLibrariesApplication.class, args);
+    }
+
+}
